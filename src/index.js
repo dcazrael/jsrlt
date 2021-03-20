e8b15b8f008cfcb1928badfedc38a8180c1dde53
dcazrael <msachdev@live.de>
2021-03-20 23:09:19 +0900

    part 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import "./lib/canvas.js";
+import { createDungeon } from "./lib/dungeon.js";
+import { Move } from "./state/components.js";
+import { player } from "./state/ecs.js";
+import { movement } from "./systems/movement.js";
+import { render } from "./systems/render.js";
+
+//init game map and player position
+const dungeon = createDungeon();
+player.position.x = dungeon.center.x;
+player.position.y = dungeon.center.y;
+
+render();
+
+document.addEventListener("keydown", (e) => {
+  processUserInput(e.key);
+});
+
+const processUserInput = (userInput) => {
+  if (userInput === "ArrowUp") player.add(Move, { x: 0, y: -1 });
+  if (userInput === "ArrowRight") player.add(Move, { x: 1, y: 0 });
+  if (userInput === "ArrowDown") player.add(Move, { x: 0, y: 1 });
+  if (userInput === "ArrowLeft") player.add(Move, { x: -1, y: 0 });
+
+  movement();
+  render();
+};
